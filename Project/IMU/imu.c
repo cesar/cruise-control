15c9dced371abf583380a8db65fe03acb2a7abf6
cesarcruz91 <cesarcruz91@gmail.com>
2014-04-24 04:07:06 -0400

    Workin on IMU

--- Project/IMU/imu.c
+++ Project/IMU/imu.c
@@ -1,8 +1,10 @@
-    #include <stdint.h>
+#include <stdint.h>
 
 #include <stdbool.h>
 
 #include <math.h>
+
+#include "driverlib/pin_map.h"
 
 #include "inc/hw_memmap.h"
 
@@ -15,8 +17,19 @@
 #include "driverlib/i2c.h"
 
 
-uint8_t SLAVE_ADDRESS = 0x1D,  POINTER_REGISTER = 0x00; //Address of the SDA pin on the digital thermometer and the pointer address to the temperature register.
-uint32_t DATAx0, DATAx1, DATAy0, DATAy1, DATAz0, DATAz1, resultX;
+#define GPIO_PE4_I2C2SCL  0x00041003
+
+#define GPIO_PE5_I2C2SDA  0x00041403
+
+#define MEASURE_MODE 0x08
+
+#define POWER_CTL 0x2D
+
+void write(uint8_t regAddr, uint8_t data);
+uint8_t read(uint8_t regAddr);
+
+uint8_t SLAVE_ADDRESS = 0x53; 
+uint32_t DATAx, DATAy, DATAz;
 uint8_t REGx0 = 0x32, REGx1 = 0x33, REGy0 = 0x34, REGy1 = 0x35, REGz0 = 0x36, REGz1 = 0x37;
 
 
@@ -25,53 +38,70 @@ void setupIMU() {
   SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C2);
 
   //Configure SDA and SCL
-  GPIOPinConfigure(GPIO_PB2_I2C2SCL);
-  GPIOPinConfigure(GPIO_PB3_I2C2SDA);
+  GPIOPinConfigure(GPIO_PE4_I2C2SCL);
+  GPIOPinConfigure(GPIO_PE5_I2C2SDA);
 
-  //Set PA6 as SCL
-  GPIOPinTypeI2CSCL(GPIO_PORTB_BASE, GPIO_PIN_2);
+  //Set PE4 as SCL
+  GPIOPinTypeI2CSCL(GPIO_PORTE_BASE, GPIO_PIN_4);
 
-  //Set PA7 as SDA
-  GPIOPinTypeI2C(GPIO_PORTB_BASE, GPIO_PIN_3);
+  //Set PE5 as SDA
+  GPIOPinTypeI2C(GPIO_PORTE_BASE, GPIO_PIN_5);
 
   //Configure Master,
   I2CMasterInitExpClk(I2C2_BASE, SysCtlClockGet(), false);
 
   //Enable Master
-  I2CMasterEnable(I2C1_BASE);
+  I2CMasterEnable(I2C2_BASE);
+
+  //Set the IMU to measure mode
+  write(POWER_CTL, MEASURE_MODE);
+
 }
 
 uint32_t readIMU() {
 
-  //Initiate the write sequence, send out the first register address
-  I2CMasterSlaveAddrSet(I2C2_BASE, SLAVE_ADDRESS, false );
-  I2CMasterDataPut(I2C2_BASE, REGx0);
+  // DATAx = read(REGx0);
+  // DATAx = DATAx << 8;
+  // DATAx = DATAx | read(REGx1);
+  DATAx = read(0x39);
+
+  return DATAx;
+}
+
+void write(uint8_t regAddr, uint8_t data) {
+
+  //Set the register address that will be altered
+  I2CMasterSlaveAddrSet(I2C2_BASE, SLAVE_ADDRESS, false);
+  I2CMasterDataPut(I2C2_BASE, regAddr);
   I2CMasterControl( I2C2_BASE, I2C_MASTER_CMD_SINGLE_SEND);
   while(I2CMasterBusBusy(I2C2_BASE)); //Loop until the bus is no longer busy
-  I2CSlaveACKValueSet(SLAVE_ADDRESS, true);
+
+  //Send the actual data to the IMU
+  I2CMasterSlaveAddrSet(I2C2_BASE, SLAVE_ADDRESS, false);
+  I2CMasterDataPut(I2C2_BASE, data);
+  I2CMasterControl( I2C2_BASE, I2C_MASTER_CMD_SINGLE_SEND);
+  while(I2CMasterBusBusy(I2C2_BASE)); //Loop until the bus is no longer busy
+
+}
+
+uint8_t read(uint8_t regAddr) {
+
+  //Set the register address that will be altered
+  I2CMasterSlaveAddrSet(I2C2_BASE, SLAVE_ADDRESS, false);
+  I2CMasterDataPut(I2C2_BASE, regAddr);
+  I2CMasterControl( I2C2_BASE, I2C_MASTER_CMD_SINGLE_SEND);
+  while(I2CMasterBusBusy(I2C2_BASE)); //Loop until the bus is no longer busy
 
   //Receive the first byte
   I2CMasterSlaveAddrSet(I2C2_BASE, SLAVE_ADDRESS, true);
-  I2CMasterControl(I2C2_BASE, I2C_MASTER_CMD_BURST_RECEIVE_START);
+  I2CMasterControl(I2C2_BASE, I2C_MASTER_CMD_SINGLE_RECEIVE);
   while(I2CMasterBusBusy(I2C2_BASE));
-  DATAx0 = I2CMasterDataGet(I2C2_BASE);
+  uint8_t temp = I2CMasterDataGet(I2C2_BASE);
   I2CSlaveACKValueSet(SLAVE_ADDRESS, true);
-
-  //Second Byte;
-  I2CMasterControl(I2C2_BASE, I2C_MASTER_CMD_BURST_RECEIVE_CONT);
-  while(I2CMasterBusBusy(I2C2_BASE));
-  DATAx1 = I2CMasterDataGet(I2C2_BASE);
-  I2CSlaveACKValueSet(SLAVE_ADDRESS, true);
-
-  //End of the transmission
-  I2CMasterControl(I2C2_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH);
-  while(I2CMasterBusBusy(I2C2_BASE));
-
-  resultX = DATAx0 << 8;
-  resultX= resultX | DATAx1;
-
-  return resultX;
+  return temp;
 }
+
+
 
 void setup() {
   Serial.begin(9600);
